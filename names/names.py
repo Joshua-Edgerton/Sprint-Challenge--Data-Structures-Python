edb05f414a37c2274d25104bdaf894a7f2c922ae
Joshua Edgerton <joshuaxedgerton@gmail.com>
2020-03-27 16:03:09 -0500

    Added binary search tree structure for names.py

--- names/names.py
+++ names/names.py
@@ -1,5 +1,7 @@
 import time
 
+from bst import BinarySearchTree
+
 start_time = time.time()
 
 f = open('./names/names_1.txt', 'r')
@@ -14,23 +16,24 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 
+# Initiate binary string
+nameBST = BinarySearchTree(names_1[0])
+
+# Search through list 1
+for name_1 in names_1:
+    nameBST.insert(name_1)
+# search through list 2
+for name_2 in names_2:
+    if nameBST.contains(name_2):
+        # append duplicates to data structure provided
+        duplicates.append(name_2)
+
 '''
 The original runtime of the 2 "for loops" would be O(n^2) 
 The new runtime with 1 "for loop" and an "if" statement would be O(n)
 original == 11.9 seconds
-new ==  1.6 seconds
+new ==  0.2 seconds
 '''
-# ORIGINAL code \/
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
-# ORIGINAL code /\
-
-# Updated code
-for name_1 in names_1:
-    if name_1 in names_2:
-        duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
