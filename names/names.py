678c9c585eab51b928bd0c7d8b231b29e08385b4
Joshua Edgerton <joshuaxedgerton@gmail.com>
2020-03-27 11:16:19 -0500

    names.py MVP, runtime reduced

--- names/names.py
+++ names/names.py
@@ -2,21 +2,35 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('./names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('./names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
+'''
+The original runtime of the 2 "for loops" would be O(n^2) 
+The new runtime with 1 "for loop" and an "if" statement would be O(n)
+original == 11.9 seconds
+new ==  1.6 seconds
+'''
+# ORIGINAL code \/
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+# ORIGINAL code /\
+
+# Updated code
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    if name_1 in names_2:
+        duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
